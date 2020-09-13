44c70116239bda5accfeba4f4b150337980e40d4
DESKTOP-QKII81G\hello <quocdong95@gmail.com>
2020-09-13 10:45:26 +0700

    default config

---
+++ java-basic/src/main/java/com/donglq/java_basic/AppConfig.java
@@ -0,0 +1,36 @@
+package com.donglq.java_basic;
+
+import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+    @Inject
+    DataSourceProperties dataSourceProperties;
+
+    @Named
+    static class JerseyConfig extends ResourceConfig {
+        public JerseyConfig() {
+            this.packages("com.donglq.java_basic");
+        }
+    }
+
+    @Bean
+    DataSource dataSource() {
+        DataSource dataSource = DataSourceBuilder
+                .create(this.dataSourceProperties.getClassLoader())
+                .url(this.dataSourceProperties.getUrl())
+                .username(this.dataSourceProperties.getUsername())
+                .password(this.dataSourceProperties.getPassword())
+                .build();
+        return new DataSourceSpy(dataSource);
+    }
+}
